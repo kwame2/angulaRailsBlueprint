de11f3ec1da5634349c334459425c16630a0632c
Robert Togbe <softcons99@yahoo.com>
2014-03-20 12:31:42 +0100

    My Base Rails Angularjs project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angularails::Application.config.secret_key_base = '7adc951d8aece39c75793c61ae3f33ecabd0805de5536c7e7f2fe5e40477b758e3a0ff6c32c6c7259416f15efd4f44961e75028c97151f67f20dee13e5805af0'
